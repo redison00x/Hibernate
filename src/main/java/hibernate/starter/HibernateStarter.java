4b8b5dde1c826269841840615aa2272efc8dd466
redison00x <praimebotob@gmail.com>
2025-03-12 15:40:18 +0300

    4commit

--- src/main/java/hibernate/starter/HibernateStarter.java
+++ src/main/java/hibernate/starter/HibernateStarter.java
@@ -1,17 +1,37 @@
 package hibernate.starter;
 
 
+import hibernate.starter.converter.BirthdayConverter;
+import hibernate.starter.entity.Birthday;
+import hibernate.starter.entity.Role;
+import hibernate.starter.entity.User;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateStarter {
     public static void main(String[] args) {
         Configuration cfg = new Configuration();
         cfg.configure("hibernate.cfg.xml");
+        cfg.addAttributeConverter(new BirthdayConverter() , true);
+  //      cfg.addAnnotatedClass(User.class);
+
         try (var sessionFactory = cfg.buildSessionFactory();
         var session = sessionFactory.openSession()){
-            System.out.println("ok");
+            session.beginTransaction();
 
+            User user = User.builder()
+                            .username("admin@mail.ru")
+                            .firstname("admin")
+                            .lastname("adminov")
+                            .birthDate(new Birthday(LocalDate.of(2000, 4, 3)))
+                            .role(Role.ADMIN)
+                    .build();
+
+            session.save(user);
+            session.update(user);
+            session.saveOrUpdate(user);
+            session.getTransaction().commit();
         }
-
     }
 }
---
+++ src/main/java/hibernate/starter/entity/User.java
@@ -0,0 +1,29 @@
+package hibernate.starter.entity;
+
+import hibernate.starter.converter.BirthdayConverter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+
+    @Convert(converter = BirthdayConverter.class)
+    @Column(name = "birth_date")
+    private Birthday birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
